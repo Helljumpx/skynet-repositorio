a1ffa891e4cf0c46cf53ec96656dc4913ffb3732
Helljumpx <kmilo_thekiller@hotmail.com>
2026-04-29 18:40:44 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// Microservicio 1: Control de Entradas (Puerto 3001)
+document.getElementById("btn-micro-1").addEventListener("click", () => {
+    window.open("http://158.247.121.198:3001", "_blank");
+});
+
+// Microservicio 2: Gestión de Proveedores (Puerto 3002)
+document.getElementById("btn-micro-2").addEventListener("click", () => {
+    window.open("http://158.247.121.198:3002", "_blank");
+});
+
+// Microservicio 3: Análisis Predictivo (Puerto 3003)
+document.getElementById("btn-micro-3").addEventListener("click", () => {
+    window.open("http://158.247.121.198:3003", "_blank");
+});
